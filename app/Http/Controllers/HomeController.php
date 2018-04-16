4aab0a90065a8c56ca1b85bcb52927317ae29590
Muhammad Alfian Rasyidin <13516104@std.stei.itb.ac.id>
2018-04-16 19:11:03 +0700

    init before work

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,21 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \RecursiveIteratorIterator;
+use \RecursiveArrayIterator;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class HomeController extends Controller
 {
     public function index(){
-        $x = "HelloWorldsss";
-        $process = new Process("python3 hello.py $x");
+        $process = new Process("python3 hello.py");
         $process->run();
 
         // executes after the command finishes
         if (!$process->isSuccessful()) {
             throw new ProcessFailedException($process);
         }
+        $json = json_encode($process->getOutput());
+        // dd($json);
+        $jsonIterator = new RecursiveIteratorIterator(
+            new RecursiveArrayIterator($json),
+            RecursiveIteratorIterator::SELF_FIRST);
 
-        echo $process->getOutput();
+        foreach ($jsonIterator as $key => $val) {
+            if(is_array($val)) {
+                echo "$key:\n";
+            } else {
+                echo "$key => $val\n";
+            }
+        }
     }
 }

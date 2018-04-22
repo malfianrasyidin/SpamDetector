5a753ea2644e20dbb4abda23ff400b5cdce30cd2
priagungs <priagungsatyagama@gmail.com>
2018-04-22 19:26:30 +0700

    added multiple keywords compatibility

--- public/Program.py
+++ public/Program.py
@@ -25,15 +25,27 @@ for tweet in data_twitter:
     temp['message'] = tweet['text']
     temp['created_at'] = TimeToString(ParseTimeFromStatus(tweet['created_at']))
     if(data['algorithm'] == 1):
-        flag, new_text = matchBM(temp['message'], data['spam_keyword'])
+        keywords = data['spam_keyword'].split(', ')
+        for keyword in keywords:
+            flag = False
+            flag2, new_text = matchBM(temp['message'], keyword)
+            flag = flag or flag2
         temp['spam_flag'] = int(flag)
         temp['message'] = new_text
     elif(data['algorithm'] == 2):
-        flag, new_text = matchBM(temp['message'], data['spam_keyword'])
+        keywords = data['spam_keyword'].split(', ')
+        for keyword in keywords:
+            flag = False
+            flag2, new_text = matchBM(temp['message'], keyword)
+            flag = flag or flag2
         temp['spam_flag'] = int(flag)
         temp['message'] = new_text
     else:
-        flag, new_text = matchBM(temp['message'], data['spam_keyword'])
+        keywords = data['spam_keyword'].split(', ')
+        for keyword in keywords:
+            flag = False
+            flag2, new_text = matchBM(temp['message'], keyword)
+            flag = flag or flag2
         temp['spam_flag'] = int(flag)
         temp['message'] = new_text
     result.append(temp)
